4cbcb823592e4023cd4193b592b93731f86c742a
Eugene Chew <eugenehjchew@gmail.com>
2022-12-29 13:05:02 +1300

    [New] script: Added installed script for initial setup

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git clone --bare git@github.com:eugenechew1/dotfiles.git $HOME/dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
