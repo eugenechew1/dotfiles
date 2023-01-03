5c995155b7877b5ec0e4f28852177ac407134ba8
Eugene Chew <eugenehjchew@gmail.com>
2023-01-04 09:57:45 +1300

    [Update] nvim/keymaps: Added additional keymap

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -57,6 +57,9 @@ keymap("n", "<leader>Y", "\"+Y")
 
 keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+keymap("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
 -- #######################################################################################
 
 -- Insert --
