a0b982b95445020f97cccf4a5ebefdcdfe1bccfd
Eugene Chew <eugenehjchew@gmail.com>
2023-01-04 09:59:24 +1300

    [Update] nvim/keymap: Added format buff keymap

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -59,6 +59,8 @@ keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 keymap("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+keymap("n", "<leader>f", vim.lsp.buf.format)
+
 
 -- #######################################################################################
 
