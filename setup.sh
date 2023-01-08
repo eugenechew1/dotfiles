787e5015db51a51067ab30bd1d932c838eecbc04
Eugene <eugenehjchew@gmail.com>
2023-01-08 21:35:30 +1300

    [Update] setup: Added script to clone z

--- setup.sh
+++ setup.sh
@@ -2,10 +2,16 @@
 
 # TODO:
 # - install z
+git clone git@github.com:rupa/z.git
+
+if [ ! -d .z ]; then
+	git clone git@github.com:rupa/z.git $HOME/.z
+fi
 # - install bat
 # - Install starship
 # - install nvim
 # - install tilix
 # - install tmux
+
 
 # EOF
