b3054036e8ae08315b4d568d7db95781d8b3d44b
Eugene Chew <eugenehjchew@gmail.com>
2023-01-13 12:09:38 +1300

    [Update] nvim/keymaps: keymap for deleting buffer

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -48,6 +48,7 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<leader>bd", ":bd", opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
