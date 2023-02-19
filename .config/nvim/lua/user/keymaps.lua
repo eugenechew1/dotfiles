6d5b5c62709a04a35f27485cf45254b717f79615
ehjc1 <eugenechew2012@gmail.com>
2023-02-19 15:46:48 +1300

    [Update] nvim/keymaps: Added window quit keymap

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -29,6 +29,7 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-q>", "<C-w>q", opts)
 
 -- Split window
 keymap("n", "ss", ":split<Return><C-w>w")
