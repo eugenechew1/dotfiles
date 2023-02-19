b2312ab0eb74f8a4d47089237ffbe55d81777018
ehjc1 <eugenechew2012@gmail.com>
2023-02-19 15:48:27 +1300

    [Update] keybind: Removed <C-k> keybind

--- .config/nvim/lua/user/plugin_config/toggleterm.lua
+++ .config/nvim/lua/user/plugin_config/toggleterm.lua
@@ -4,26 +4,26 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
+	--[[ size = 20, ]]
 	open_mapping = [[<c-\>]],
-	hide_numbers = true,
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 2,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	direction = "float",
-	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-		winblend = 0,
-		highlights = {
-			border = "Normal",
-			background = "Normal",
-		},
-	},
+	--[[ hide_numbers = true, ]]
+	--[[ shade_filetypes = {}, ]]
+	--[[ shade_terminals = true, ]]
+	--[[ shading_factor = 2, ]]
+	--[[ start_in_insert = true, ]]
+	insert_mappings = false,
+	--[[ persist_size = true, ]]
+	--[[ direction = "float", ]]
+	--[[ close_on_exit = true, ]]
+	--[[ shell = vim.o.shell, ]]
+	--[[ float_opts = { ]]
+	--[[ 	border = "curved", ]]
+	--[[ 	winblend = 0, ]]
+	--[[ 	highlights = { ]]
+	--[[ 		border = "Normal", ]]
+	--[[ 		background = "Normal", ]]
+	--[[ 	}, ]]
+	--[[ }, ]]
 })
 
 function _G.set_terminal_keymaps()
