dd2492733e533d26fa67d0300b0b6f47927b1213
eugenechew1 <eugenehjchew@gmail.com>
2025-03-21 23:55:04 +1300

    [Fix] config: Fixed errors

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -56,7 +56,7 @@ return {
       })
 
       -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-      require("ts_context_commentstring").setup({})
+      -- require("ts_context_commentstring").setup({})
     end,
   },
 }
