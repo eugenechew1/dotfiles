fe7807fc95871fd44c61021e906677026a9cb8b7
eugenechew1 <eugenehjchew@gmail.com>
2025-04-11 09:15:09 +1200

    [Update] nvim: added error condition for lazy module

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -6,54 +6,59 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
-require("lazy").setup({
-  spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    -- LINTING EXTRAS
-    { import = "lazyvim.plugins.extras.linting.eslint" },
+local lazy = require("lazy")
+if lazy then
+  require("lazy").setup({
+    spec = {
+      -- add LazyVim and import its plugins
+      { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+      -- import any extras modules here
+      -- LINTING EXTRAS
+      { import = "lazyvim.plugins.extras.linting.eslint" },
 
-    -- FORMATTING EXTRAS
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.formatting.black" },
+      -- FORMATTING EXTRAS
+      { import = "lazyvim.plugins.extras.formatting.prettier" },
+      { import = "lazyvim.plugins.extras.formatting.black" },
 
-    -- LANGUAGE EXTRAS
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    -- { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.cmake" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    -- NOTE: This is where we import our custom `plugins`
-    -- To add new plugins navigate to `../plugins/<custom-plugin>.lua`
-    -- import/override with your plugins
-    { import = "plugins" },
-  },
-  defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
+      -- LANGUAGE EXTRAS
+      { import = "lazyvim.plugins.extras.lang.typescript" },
+      { import = "lazyvim.plugins.extras.lang.rust" },
+      { import = "lazyvim.plugins.extras.lang.python" },
+      -- { import = "lazyvim.plugins.extras.lang.go" },
+      { import = "lazyvim.plugins.extras.lang.cmake" },
+      { import = "lazyvim.plugins.extras.lang.json" },
+      -- NOTE: This is where we import our custom `plugins`
+      -- To add new plugins navigate to `../plugins/<custom-plugin>.lua`
+      -- import/override with your plugins
+      { import = "plugins" },
+    },
+    defaults = {
+      -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+      -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+      lazy = false,
+      -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+      -- have outdated releases, which may break your Neovim install.
+      version = false, -- always use the latest git commit
+      -- version = "*", -- try installing the latest stable version for plugins that support semver
+    },
+    install = { colorscheme = { "tokyonight", "habamax" } },
+    checker = { enabled = true }, -- automatically check for plugin updates
+    performance = {
+      rtp = {
+        -- disable some rtp plugins
+        disabled_plugins = {
+          "gzip",
+          -- "matchit",
+          -- "matchparen",
+          -- "netrwPlugin",
+          "tarPlugin",
+          "tohtml",
+          "tutor",
+          "zipPlugin",
+        },
       },
     },
-  },
-})
+  })
+else
+  print("Failed to load lazy modules")
+end
